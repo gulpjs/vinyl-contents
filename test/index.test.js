be2c3c0f151c1254e66361e731809373df87e754
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-10-08 16:11:20 -0700

    feat!: Drop readable-stream & use bl directly (#8)
    feat!: Upgrade bl
    chore: Add separate tests for streamx, readable-stream & core streams
    chore: Update example to use streamx
    feat!: Update vinyl dependency
    chore: Rework tests and add a file clone test

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,6 @@
 
 var expect = require('expect');
 var Vinyl = require('vinyl');
-var from = require('from2');
 
 var vinylContents = require('../');
 
@@ -17,24 +16,6 @@ function makeBufferFile() {
   return new Vinyl({
     path: 'test.js',
     contents: Buffer.from('var a = 1;'),
-  });
-}
-
-function makeStreamFile() {
-  return new Vinyl({
-    path: 'test.js',
-    contents: from([
-      Buffer.from('var a'),
-      Buffer.from(' = '),
-      Buffer.from('1;'),
-    ]),
-  });
-}
-
-function makeErrorStreamFile() {
-  return new Vinyl({
-    path: 'test.js',
-    contents: from([new Error('boom!')]),
   });
 }
 
@@ -59,26 +40,6 @@ describe('vinyl-contents', function () {
     });
   });
 
-  it('returns the contents of a Vinyl file with Streaming contents', function (done) {
-    var file = makeStreamFile();
-
-    vinylContents(file, function (err, contents) {
-      expect(err).toBeFalsy();
-      expect(contents.toString()).toEqual(expectedBuffer.toString());
-      done();
-    });
-  });
-
-  it('works with String(contents)', function (done) {
-    var file = makeStreamFile();
-
-    vinylContents(file, function (err, contents) {
-      expect(err).toBeFalsy();
-      expect(String(contents)).toEqual(expectedBuffer.toString());
-      done();
-    });
-  });
-
   it('returns empty contents if Vinyl file has no contents', function (done) {
     var file = makeEmptyFile();
 
@@ -89,13 +50,122 @@ describe('vinyl-contents', function () {
     });
   });
 
-  it('surfaces errors within content stream', function (done) {
-    var file = makeErrorStreamFile();
+  function streamSuite(moduleName) {
+    var stream = require(moduleName);
 
-    vinylContents(file, function (err, contents) {
-      expect(err).toBeInstanceOf(Error);
-      expect(contents).toBeUndefined();
-      done();
+    function makeStreamFile() {
+      return new Vinyl({
+        path: 'test.js',
+        contents: stream.Readable.from([
+          Buffer.from('var a'),
+          Buffer.from(' = '),
+          Buffer.from('1;'),
+        ]),
+      });
+    }
+
+    function makeImmediateErrorStreamFile() {
+      var contents = new stream.Readable({
+        read: function (cb) {
+          var err = new Error('boom!');
+          if (typeof cb === 'function') {
+            return cb(err);
+          }
+
+          this.destroy(err);
+        },
+      });
+
+      return new Vinyl({
+        path: 'test.js',
+        contents: contents,
+      });
+    }
+
+    function makeErrorStreamFile() {
+      var items = [Buffer.from('var a'), Buffer.from(' = '), Buffer.from('1;')];
+      var contents = new stream.Readable({
+        read: function (cb) {
+          var chunk = items.shift();
+          if (chunk) {
+            this.push(chunk);
+            if (typeof cb === 'function') {
+              cb();
+            }
+            return;
+          }
+
+          var err = new Error('boom!');
+          if (typeof cb === 'function') {
+            return cb(err);
+          }
+
+          this.destroy(err);
+        },
+      });
+
+      return new Vinyl({
+        path: 'test.js',
+        contents: contents,
+      });
+    }
+
+    describe('with (' + moduleName + ')', function () {
+      it('returns the contents of a Vinyl file with Streaming contents', function (done) {
+        var file = makeStreamFile();
+
+        vinylContents(file, function (err, contents) {
+          expect(err).toBeFalsy();
+          expect(contents.toString()).toEqual(expectedBuffer.toString());
+          done();
+        });
+      });
+
+      it('works with String(contents)', function (done) {
+        var file = makeStreamFile();
+
+        vinylContents(file, function (err, contents) {
+          expect(err).toBeFalsy();
+          expect(String(contents)).toEqual(expectedBuffer.toString());
+          done();
+        });
+      });
+
+      it('surfaces immediate errors within content stream', function (done) {
+        var file = makeImmediateErrorStreamFile();
+
+        vinylContents(file, function (err, contents) {
+          expect(err).toBeInstanceOf(Error);
+          expect(contents).toBeUndefined();
+          done();
+        });
+      });
+
+      it('surfaces errors anywhere within content stream', function (done) {
+        var file = makeErrorStreamFile();
+
+        vinylContents(file, function (err, contents) {
+          expect(err).toBeInstanceOf(Error);
+          expect(contents).toBeUndefined();
+          done();
+        });
+      });
+
+      it('works with a cloned file that has streaming contents', function (done) {
+        var file = makeStreamFile();
+
+        var file2 = file.clone();
+
+        vinylContents(file2, function (err, contents) {
+          expect(err).toBeFalsy();
+          expect(contents.toString()).toEqual(expectedBuffer.toString());
+          done();
+        });
+      });
     });
-  });
+  }
+
+  streamSuite('stream');
+  streamSuite('streamx');
+  streamSuite('readable-stream');
 });
